8f6afd5146508ae928a7a81516097b3be3776dcb
gixorian <goranborovecki1@gmail.com>
2024-01-13 08:30:15 +0100

    python

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,6 +22,12 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
